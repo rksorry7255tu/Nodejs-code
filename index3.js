20729eb2a0d5a56cb085114415145dfe799ebe19
rksorry7255tu <rk7255053708@gmail.com>
2024-05-27 22:16:28 +0530

    CRUD operation

---
+++ index3.js
@@ -0,0 +1,42 @@
+//creating a folder 
+
+ const file = require("fs");
+file.mkdir("Ravi",(error)=>{
+    console.log(error);
+});
+
+//creating a file inside it
+
+file.writeFile("./Ravi/bio.txt","Hi!,I am Ravi kumar",(error)=>{
+    console.log(error);
+});
+
+//to add more data at the end of the file
+
+file.appendFile("./Ravi/bio.txt"," hello this is the appended data by the userrrr",(error)=>{
+    console.log(error);
+});
+
+
+//read the data 
+
+const data=file.readFile("./Ravi/bio.txt","utf-8",(error,data)=>{
+    console.log(error);
+    console.log(data);
+});
+
+//rename the file name as mybio.txt
+
+file.rename("./Ravi/bio.txt","./Ravi/mybio.txt",(error)=>{
+    console.log(error);
+});
+
+//delete both file and folder
+
+file.unlink("./Ravi/mybio.txt",(error)=>{
+    console.log(error);
+});
+
+
+
+
