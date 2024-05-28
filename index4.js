1db5b71467959f468d5df71c2de743af081e8afc
rksorry7255tu <rk7255053708@gmail.com>
2024-05-29 00:09:03 +0530

    Import and export our own modules

---
+++ index4.js
@@ -0,0 +1,7 @@
+const {add,sub,name}=require(`./operator`);
+
+
+console.log(add(5,5));
+console.log(sub(10,5));
+ console.log(name);
+
---
+++ operator.js
@@ -0,0 +1,14 @@
+
+const add=(a,b)=>{
+    return a+b;
+};
+const sub=(a,b)=>{
+    return a-b;
+}
+const name = "Ravi";
+// module.exports.sub=sub;
+// module.exports.add= add;
+
+// module.exports.name=name;
+
+module.exports={sub,add,name};
