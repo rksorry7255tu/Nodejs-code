4efae7c0bdfd844bdf04f31a0190cda64bc56b6b
rksorry7255tu <rk7255053708@gmail.com>
2024-06-02 12:11:07 +0530

    Streaming Codes

---
+++ streaming.js
@@ -0,0 +1,21 @@
+
+const fs = require("fs");
+const http = require("http");
+const server= http.createServer();
+server.on(`request`,(req,res)=>{
+  
+    // fs.readFile(`input.txt`,(error,data)=>{
+    //   res.end(data.toString());
+    // });
+    const rstream = fs.createReadStream(`input.txt`);
+     rstream.on(`data`,(chunkdata)=>{
+         res.write(chunkdata);
+     });
+     rstream.on("end",()=>{
+         res.end();
+     });
+     rstream.on("error",()=>{
+         console.log(error);
+     })
+});
+server.listen(3000);
